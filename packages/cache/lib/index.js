2e1c15de7cbf3c78b098054c468d1a17c0f51a9f
Sasha <aleksandr.gulchenko@gmail.com>
2020-10-08 10:42:15 +0300

    initial

---
+++ packages/cache/lib/index.js
@@ -0,0 +1,10 @@
+import persistReducer, { reset } from './persist'
+import cacheMiddleware from './cache-middleware'
+import CheckCache from './CheckCache'
+
+export {
+  persistReducer,
+  cacheMiddleware,
+  reset,
+  CheckCache,
+}
