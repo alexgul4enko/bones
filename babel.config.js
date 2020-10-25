051a3a5517e2984c254f1fec4468bfa08f7bef99
Sasha <aleksandr.gulchenko@gmail.com>
2020-10-25 11:00:35 +0200

    babel

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        useBuiltIns: 'usage',
+        useBuiltIns: 'entry',
         corejs: {
           version: 3,
           proposals: true,
