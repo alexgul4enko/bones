aea6f314f88219cc233702c242830cdde0f19bcc
Sasha <aleksandr.gulchenko@gmail.com>
2020-10-26 17:46:57 +0200

    fix prefetch resource

--- packages/resource/lib/hooks/usePrefetchRequest.js
+++ packages/resource/lib/hooks/usePrefetchRequest.js
@@ -1,9 +1,12 @@
 import { useEffect } from 'react'
+import useClear from './useClear'
 import { useCustomRequest } from './useRequest'
+import { getNameSpace } from '../utils'
 import get from 'lodash/get'
 
 export default function usePrefetchRequest(asyncFunc, config, prefetchConfigs) {
   const resource = useCustomRequest(asyncFunc, config)
+  const clear = useClear(getNameSpace(get(config, 'namespace', config)))
   useEffect(() => {
     const request = resource.request(
       get(prefetchConfigs, 'filters'),
@@ -12,7 +15,7 @@ export default function usePrefetchRequest(asyncFunc, config, prefetchConfigs) {
     if(get(prefetchConfigs, 'destroyOnUnmount', true)) {
       return () => {
         request.cancel()
-        actions.clear()
+        clear()
       }
     }
     return request.cancel
