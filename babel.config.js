b6a6df66a473240b24db3c0434929e40254fb8bd
Sasha <aleksandr.gulchenko@gmail.com>
2021-03-02 21:21:29 +0200

    bump versions

--- babel.config.js
+++ babel.config.js
@@ -11,22 +11,20 @@ module.exports = {
         },
       },
     ],
-    '@babel/preset-react',
+    ["@babel/preset-react", {
+      "runtime": "automatic"
+    }]
     '@babel/preset-flow',
   ],
   plugins: [
     // stage 2
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    // '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-
     // stage 3
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
     ['@babel/plugin-proposal-class-properties', { loose: false }],
     '@babel/plugin-proposal-json-strings',
     '@babel/plugin-proposal-export-namespace-from',
-    'babel-plugin-react-require',
   ],
 }
--- packages/cache/lib/CheckCache.js
+++ packages/cache/lib/CheckCache.js
@@ -1,4 +1,14 @@
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+
+CheckCache.propTypes = {
+  isInitialized: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+}
+
+CheckCache.defaultProps = {
+  isInitialized: false,
+}
 
 function CheckCache({ isInitialized, children }) {
   if(!isInitialized) {
